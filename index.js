2509747873b12322bb92e46d98b1215c6c229cbc
Leslie Zimmermann <lesliez@gmx.de>
2020-01-24 11:33:38 +0100

    enable plugin in production

--- index.js
+++ index.js
@@ -7,66 +7,81 @@ const prettyMs = require('pretty-ms');
 const glob = require('glob');
 
 const getMetaTag = (html, property) => {
-	const regex = new RegExp(`<meta[^>]*property=["|']${property}["|'][^>]*>`, 'i');
-	const regexExec = regex.exec(html);
-	if (regexExec) {
-		return regexExec[0];
-	}
-	return false;
+  const regex = new RegExp(
+    `<meta[^>]*(property|name)=["|']${property}["|'][^>]*>`,
+    'i'
+  );
+  const regexExec = regex.exec(html);
+
+  if (regexExec) {
+    return regexExec[0];
+  }
+  return false;
 };
 
 const getMetaTagContent = metaTagHtml => {
-	const regex = /content=["]([^"]*)["]/i;
-	const regexExec = regex.exec(metaTagHtml);
-	if (regexExec) {
-		return regexExec[1];
-	}
-	return false;
+  const regex = /content=["]([^"]*)["]/i;
+  const regexExec = regex.exec(metaTagHtml);
+
+  if (regexExec) {
+    return regexExec[1];
+  }
+  return false;
 };
 
 module.exports = bundler => {
-	bundler.on('buildEnd', async () => {
-		if (process.env.NODE_ENV !== 'production') {
-			return;
-		}
-		console.log('');
-		const spinner = ora(chalk.grey('Fixing og:image link')).start();
-		const start = Date.now();
+  bundler.on('buildEnd', async () => {
+    const spinner = ora(chalk.grey('Fixing meta tags image links')).start();
+    const start = Date.now();
 
-		glob.sync(`${bundler.options.outDir}/**/*.html`).forEach(file => {
-			const htmlPath = path.resolve(file);
-			const html = fs.readFileSync(htmlPath).toString();
-			const twitterImageTag = getMetaTag(html, 'twitter:image');
-			const ogImageTag = getMetaTag(html, 'og:image');
-			const ogUrlTag = getMetaTag(html, 'og:url');
+    glob.sync(`${bundler.options.outDir}/**/*.html`).forEach(file => {
+      const htmlPath = path.resolve(file);
+      const html = fs.readFileSync(htmlPath).toString();
+      let patchedHtml = html;
 
-			if (ogUrlTag) {
-				if (ogImageTag) {
-					const ogImageContent = getMetaTagContent(ogImageTag);
-					const ogUrlContent = getMetaTagContent(ogUrlTag);
-					const absoluteOgImageUrl = url.resolve(ogUrlContent, ogImageContent);
-					const ogImageTagAbsoluteUrl = ogImageTag.replace(ogImageContent, absoluteOgImageUrl);
-					const patchedHtml = html.replace(ogImageTag, ogImageTagAbsoluteUrl);
+      const ogUrlTag = getMetaTag(html, 'og:url');
+      const ogImageTag = getMetaTag(html, 'og:image');
+      const twitterImageTag = getMetaTag(html, 'twitter:image');
 
-					fs.writeFileSync(htmlPath, patchedHtml);
-				}
+      if (ogUrlTag) {
+        const ogUrlContent = getMetaTagContent(ogUrlTag);
 
-				if (twitterImageTag) {
-					const ogImageContent = getMetaTagContent(twitterImageTag);
-					const ogUrlContent = getMetaTagContent(ogUrlTag);
-					const absoluteTwitterImageUrl = url.resolve(ogUrlContent, ogImageContent);
-					const twitterImageTagAbsoluteUrl = twitterImageTag.replace(ogImageContent, absoluteTwitterImageUrl);
-					const patchedHtml = html.replace(twitterImageTag, twitterImageTagAbsoluteUrl);
+        if (ogImageTag) {
+          const ogImageContent = getMetaTagContent(ogImageTag);
+          const absoluteOgImageUrl = url.resolve(ogUrlContent, ogImageContent);
+          const ogImageTagAbsoluteUrl = ogImageTag.replace(
+            ogImageContent,
+            absoluteOgImageUrl
+          );
+          patchedHtml = patchedHtml.replace(ogImageTag, ogImageTagAbsoluteUrl);
+        }
 
-					fs.writeFileSync(htmlPath, patchedHtml);
-				}
-			}
-		});
+        if (twitterImageTag) {
+          const twitterImageContent = getMetaTagContent(twitterImageTag);
+          const absoluteTwitterImageUrl = url.resolve(
+            ogUrlContent,
+            twitterImageContent
+          );
+          const twitterImageTagAbsoluteUrl = twitterImageTag.replace(
+            twitterImageContent,
+            absoluteTwitterImageUrl
+          );
+          patchedHtml = patchedHtml.replace(
+            twitterImageTag,
+            twitterImageTagAbsoluteUrl
+          );
+        }
+      }
 
-		const end = Date.now();
-		spinner.stopAndPersist({
-			symbol: '✨ ',
-			text: chalk.green(`Fixed og:image and twitter:image links in ${prettyMs(end - start)}.`)
-		});
-	});
+      fs.writeFileSync(htmlPath, patchedHtml);
+    });
+
+    const end = Date.now();
+    spinner.stopAndPersist({
+      symbol: '✨ ',
+      text: chalk.green(
+        `Fixed meta tags image links in ${prettyMs(end - start)}.`
+      )
+    });
+  });
 };
